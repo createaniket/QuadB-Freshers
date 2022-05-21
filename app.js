06f39786cc4c178ae7b5a10a50c1f64fc97b2fb2
Aniket <aniketpratapsingh7310@gmail.com>
2022-05-21 18:15:26 +0530

    :

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const Readroute = require('./read')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 const rootsrc = path.join(__dirname, './resources')
 app.use(express.static(rootsrc))
 const viewspath = path.join(__dirname, './Templates/views')
